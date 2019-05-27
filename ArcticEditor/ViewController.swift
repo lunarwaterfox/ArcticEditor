e615c505bcb7407df62e807251f424cfe9d42a79
lunarwaterfox <lunarwaterfox@gmail.com>
2019-05-27 16:18:55 +0800

    create project

---
+++ ArcticEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArcticEditor
+//
+//  Created by lcl on 2019/5/27.
+//  Copyright © 2019 vuples. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
